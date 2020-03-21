5ef916075f4df87427f1565794114333f76bea77
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2020-03-21 23:10:26 +0100

    created user model

---
+++ app/models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+const userSchema = new schema({
+    firstName:{type:String , required:true},
+    lastName:{type:String , required:true},
+    address:{type:String , required:true},
+    email:{type:String , required:true},
+    phoneNumber:{type:String , required:true},
+    statusCode:{type:Number},
+    publicId:{type:mongoose.SchemaTypes.ObjectId},
+    passwordToken:{type:Number },
+    password: { type: String, required: true },
+    status: { type: Boolean },
+    imageUrl: { type: String, default: '' },
+    imageID: { type: String, default: '' },
+    userType:{type:String , default:''},
+    CreatedAt: { type: Date }
+
+}) 
+
+module.exports = mongoose.model('user', userSchema)
