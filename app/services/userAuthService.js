d0f7f43b925df19904e2d5bdc2083e48749f6328
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2020-03-21 23:13:13 +0100

    created user registration method

---
+++ app/services/userAuthService.js
@@ -0,0 +1,49 @@
+const model = require('../models/user');
+const bcrypt = require('bcryptjs')
+const mongoose = require('mongoose');
+const mailer = require('../middleware/mailer');
+exports.registerUser = (option)=>{
+
+return new Promise((resolve , reject)=>{
+        model.findOne({email:option.email}).then(exists =>{
+            const hash = bcrypt.hashSync(option.password , 10)
+            const gen = Math.floor(1000 + Math.random() * 9000);
+
+            if(exists){
+                resolve({success:false , message:'Sorry user already exists !!!'})
+            }else{
+                const userDetails = {
+                    firstName:option.firstName,
+                    lastName:option.lastName,
+                    address:option.address,
+                    email:option.email,
+                    phoneNumber:option.phoneNumber,
+                    publicId:mongoose.Types.ObjectId(),
+                    statusCode:gen ,
+                    passwordToken:'',
+                    password:hash ,
+                    status: false,
+                    userType:'client',
+                }
+                model.create(userDetails).then(created =>{
+                    if(created){
+                        mailer.SignUpMail(userDetails.email ,userDetails.statusCode).then(sent =>{
+                            if(sent){
+                                resolve({success:true , message:'user registration was successfull !!!'})
+                            }else{
+                                resolve({success:false , message:'Error occured while trying to register user !!!'})
+                            }
+                        })
+                    }else{
+                        resolve({success:false , message:'user registration was not successfull !!!'})
+                    }
+                }).catch(err =>{
+                    reject(err);
+                })
+            }
+        }).catch(err =>{
+            reject(err);
+        })
+ 
+})
+}
