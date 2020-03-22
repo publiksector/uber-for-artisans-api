c3492c01d0c2f1cbf3f762b5898f9181fbcad57d
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2020-03-22 12:38:36 +0100

    user login routes

--- app/routes/userRoutes.js
+++ app/routes/userRoutes.js
@@ -6,6 +6,7 @@ module.exports = function(){
     const authCtrl = new authController();
     router.post('/register', authCtrl.registerUser);
     router.put('/verify', authCtrl.verifyUser);
+    router.post('/authenticate', authCtrl.loginUser);
 
     return router;
 }
