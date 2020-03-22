ec1153208a08355dc31e2a8a492bfe2aafbace37
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2020-03-22 01:33:34 +0100

    modified

--- app/controllers/userAuthController.js
+++ app/controllers/userAuthController.js
@@ -1,19 +1,25 @@
-const service = require('../services/userAuthService');
+const service = require("../services/userAuthService");
 
-module.exports = function userAuthController (){
-    this.registerUser = (req,res)=>{
-        service.registerUser(req.body).then(data =>{
-            res.json(data)
-        }).catch(err =>{
-            res.json(err);
-        })
-    }
+module.exports = function userAuthController() {
+  this.registerUser = (req, res) => {
+    service
+      .registerUser(req.body)
+      .then(data => {
+        res.json(data);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  };
 
-    this.verifyUser = (req,res)=>{
-        service.verifyUser(req.body).then(data =>{
-            res.json(data)
-        }).catch(err =>{
-            res.json(err);
-        })
-    }
-}
+  this.verifyUser = (req, res) => {
+    service
+      .verifyUser(req.body)
+      .then(data => {
+        res.json(data);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  };
+};
