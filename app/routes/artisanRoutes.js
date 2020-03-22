83c0e2d348e74b43f97b5eae8433de8010b62b48
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2020-03-22 12:38:19 +0100

    artisan login routes

--- app/routes/artisanRoutes.js
+++ app/routes/artisanRoutes.js
@@ -5,6 +5,7 @@ var router = require('express').Router();
 module.exports = function(){
     const authCtrl = new authController();
     router.post('/register', authCtrl.registerArtisan);
+    router.post('/authenticate', authCtrl.artisanLogin);
 
     return router;
 }
