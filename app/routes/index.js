7641b4c77a9d6b1cfe1147f67e376151634cfc1b
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2020-03-22 01:35:24 +0100

    added artisan routes to index routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,8 @@
-var UserRoutes = require('./userRoutes');
+const userRoutes = require('./userRoutes');
+const artisanRoutes = require('./artisanRoutes');
 
 module.exports = function (router) {
-    router.use('/user', UserRoutes())
+    router.use('/user', userRoutes())
+    router.use('/artisan', artisanRoutes())
     return router;
 }
