d34826cf387cf14476acca21edbcd7cdfcf93cf2
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2020-03-23 16:14:22 +0100

    added update client profile

--- app/controllers/userAuthController.js
+++ app/controllers/userAuthController.js
@@ -1,5 +1,5 @@
 const service = require("../services/userAuthService");
-
+const cloudinary = require('../middleware/cloudinary')
 module.exports = function userAuthController() {
   this.registerUser = (req, res) => {
     service
@@ -32,6 +32,33 @@ module.exports = function userAuthController() {
       })
       .catch(err => {
         res.json(err);
+      });
+  };
+
+  this.updateClientProfile = async (req, res) => {
+    console.log("checking file content", req.file );
+    var requestDetails = {
+      image: req.file != null && req.file !== undefined ? req.file.path : null
+    };
+
+    console.log("file detail recieved", requestDetails.image);
+    if (req.image !== null && req.file !== undefined) {
+      await cloudinary.uploadToCloud(requestDetails.image).then(img => {
+        console.log("Cloudinary details recieved", img.url);
+        requestDetails.imageUrl = img.url;
+        requestDetails.imageID = img.ID;
+        return requestDetails;
+      });
+    }
+
+    console.log("calling outside await");
+    service
+      .updateProfile(req.auth.publicId, requestDetails)
+      .then(data => {
+        res.status(200).send(data);
+      })
+      .catch(err => {
+        res.status(500).send(err);
       });
   };
 };
