f500363d7d9870d9a3f5402cab130b8f674965a2
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2020-03-23 16:15:17 +0100

    modified artisan model

--- app/models/artisan.js
+++ app/models/artisan.js
@@ -2,20 +2,22 @@ const mongoose = require('mongoose');
 const schema = mongoose.Schema;
 const artisanSchema = new schema({
     userId: { type: mongoose.Types.ObjectId, ref: 'user', autopopulate: true },
+    dob:{type:Date , default:''},
+    bvn:{type:Number , default:''},
     services:[{type:mongoose.Types.ObjectId, ref:'services',  autopopulate: true }],
     review: [{
         userId: { type: mongoose.Types.ObjectId, ref: 'user', autopopulate: true },
         appointmentId: { type: String, ref: 'booking', autopopulate: true, },
         message: { type: String },
-        CreatedAt: { type: Date, default: Date.now }
+        createdAt: { type: Date, default: Date.now }
     }],
     ratings: [{
         rating: { type: Number },
         userId: { type: mongoose.SchemaTypes.ObjectId, ref: 'user', autopopulate: true },
         appointmentId: { type: String, ref: 'booking', autopopulate: true, },
-        CreatedAt: { type: Date, default: Date.now }
+        createdAt: { type: Date, default: Date.now }
     }],
-    CreatedAt: { type: Date, default: Date.now }
+    createdAt: { type: Date, default: Date.now }
 
 }) 
 
