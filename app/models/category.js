fb84617c4009d5fc68865bca77c565b7304401cb
ochoko peter c <33673507+buka4chocksy@users.noreply.github.com>
2020-04-05 13:24:28 +0100

    Revert "New branch"

--- app/models/category.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const schema = mongoose.Schema;
-const categorySchema = new schema({
-    name:{type:String , required:true},
-    imageUrl:{type:String, default:''},
-    iconUrl:{type:String , default:''},
-    dateCreated:{type:Date , default:Date.now()}
-})
-module.exports = mongoose.model('category', categorySchema);
