e1a0b9c2eb786b2d4957ad1e253c3f70324fb0f8
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2020-04-07 16:19:22 +0100

    new modification

--- server.js
+++ server.js
@@ -26,6 +26,30 @@ app.use(cookieparser());
 app.use(cors());
 app.use('/api', rootRouter);
 
+app.use((req, res, next) => {
+        const error = new Error('Not found');
+        error.status = 404;
+        next(error);
+})
+app.use((error, req, res, next) => {
+        res.status(error.status || 500);
+        res.json({
+                error: {
+                        message: error.message
+                }
+        })
+})
+// app.get('/', function (req, res) {
+//     console.log(req.body.Ids.map(e => ObjectId(e)))
+//     _user.find({
+//         '_id': {
+//             $in: ['5e52dac65bb54de698191bcd']
+//         }
+//     }, (err, docs) => {
+//         res.json({ docs, message: "hello world" });
+//     })
+// });
+
 app.get('/', function(req, res){
         res.json({message:"hello world"});
     });
