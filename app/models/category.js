a7ccf117e215ae72ae5345497c23261f5d4db717
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2020-04-24 10:58:48 +0100

    modified categories schema

--- app/models/category.js
+++ app/models/category.js
@@ -4,6 +4,10 @@ const categorySchema = new schema({
     name:{type:String , required:true},
     imageUrl:{type:String, default:''},
     iconUrl:{type:String , default:''},
+    viewLocation:[{
+        views:{type:Number, default:0},
+        location:{type:String }
+    }],
     dateCreated:{type:Date , default:Date.now()}
 })
 module.exports = mongoose.model('category', categorySchema);
