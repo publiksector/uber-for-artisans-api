d5424f4bbf094abd938a6e0d5e1dfc0bc50dac91
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2020-08-26 00:13:31 +0100

    modified artisan model

--- app/models/artisan.js
+++ app/models/artisan.js
@@ -1,31 +1,22 @@
 const mongoose = require('mongoose');
 const schema = mongoose.Schema;
 const artisanSchema = new schema({
-    firstName:{type:String , required:true},
-    lastName:{type:String , required:true},
-    address:{type:String , required:true},
-    email:{type:String , required:true},
+    firstName:{type:String , default:""},
+    lastName:{type:String ,default:""},
+    address:{type:String  , default:""},
+    email:{type:String , default:""},
     phoneNumber:{type:String , required:true},
     publicId:{type:mongoose.Types.ObjectId},
     imageUrl: { type: String, default: '' },
+    statusCode:{type:Number},
     imageID: { type: String, default: '' },
-    
     userType:{type:String , default:'artisan'},
+    verified:{type:Boolean , default:false},
+    active: { type: Boolean  , default:false},
+    status: { type: Boolean  , default:false},
     dob:{type:Date , default:''},
     bvn:{type:Number , default:''},
     services:[{type:mongoose.Types.ObjectId, ref:'services',  autopopulate: true }],
-    review: [{
-        userId: { type: mongoose.Types.ObjectId, ref: 'user', autopopulate: true },
-        appointmentId: { type: String, ref: 'booking', autopopulate: true, },
-        message: { type: String },
-        createdAt: { type: Date, default: Date.now }
-    }],
-    ratings: [{
-        rating: { type: Number },
-        userId: { type: mongoose.SchemaTypes.ObjectId, ref: 'user', autopopulate: true },
-        appointmentId: { type: String, ref: 'booking', autopopulate: true, },
-        createdAt: { type: Date, default: Date.now }
-    }],
     lastLoggedIn:{type:Date , default:''},
     createdAt: { type: Date, default: Date.now }
 
